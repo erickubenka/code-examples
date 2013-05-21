427494acac12ea526b251f13e76a66c552226867
erickubenka <kubeneric@aol.com>
2013-05-21 23:07:56 +0200

    added protect download route

--- 2013/laravel4_protect_files/app/routes.php
+++ 2013/laravel4_protect_files/app/routes.php
@@ -1,17 +1,20 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
-
 Route::get('/', function()
-{
-	return View::make('hello');
-});
+				{
+						return View::make('hello');
+				});
+
+Route::get('download/{name}', array(function($name)
+		{
+				$filePath = storage_path().'/files/'.$name;
+				
+				if(File::exists($filePath))
+				{
+						return Response::download($filePath);
+				}
+				else
+				{
+						return "The file you're looking for doesn't exist";
+				}	
+		}));
