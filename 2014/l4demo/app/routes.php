e4ab2c3e21a4934626daf2d84cade18aab31daec
erickubenka <kubeneric@aol.com>
2014-02-25 20:11:04 +0100

    Stuff

--- 2014/l4demo/app/routes.php
+++ 2014/l4demo/app/routes.php
@@ -11,55 +11,57 @@
 |
 */
 
-Route::group(array('domain' => 'local.dev'), function ()
-{
-    Route::get('/', function ()
-    {
-        return 'local.dev';
-    });
-});
-
-Route::group(array('domain' => 'wurst.dev'), function ()
-{
-    Route::get('/', function ()
-    {
-        return 'wurst.dev';
-    });
-});
-
-/**
- * Closure Route
- */
-Route::get('/demo', function ()
-{
-    return View::make('demo.home');
-});
-
-Route::post('/demo', function(){
-   return View::make('demo.home')->with('name', Input::get('name'));
-});
-
-/**
- * Controller Action Routing
- */
-Route::get('/ctrlaction', 'HomeController@showWelcome');
-
-/**
- * RESTful Controller / Routing
- * Base Uri And Controller
- */
-Route::controller('democtrl', 'DemoController');
-
-/**
- * Resource Controller
- */
+//Route::group(array('domain' => 'local.dev'), function () {
+//    Route::get('/', function () {
+//        return 'local.dev';
+//    });
+//});
+//
+//Route::group(array('domain' => 'wurst.dev'), function () {
+//    Route::get('/', function () {
+//        return 'wurst.dev';
+//    });
+//});
+//
+//Route::get('/demo', function () {
+//    return View::make('demo.home');
+//});
+//
+//Route::post('/demo', function () {
+//    return View::make('demo.home')->with('name', Input::get('name'));
+//});
+//
+//Route::get('/ctrlaction', 'HomeController@showWelcome');
+//
+//Route::controller('democtrl', 'DemoController');
+//
 Route::resource('user', 'UserController');
+//
+//Route::get('login', function () {
+//    return View::make('demo.auth.login');
+//});
+//
+//Route::post('login', function () {
+//
+//    $creds = [
+//        'email' => Input::get('email'),
+//        'password' => Input::get('password')
+//    ];
+//
+//    if(Auth::attempt($creds)){
+//        return Redirect::to('login')->with('success', "You're logged in");
+//    }
+//
+//        return Redirect::to('login')->with('error', "Wrong credentials");
+//});
+//
+//Route::post('logout', function () {
+//    Auth::logout();
+//    return Redirect::to('login')->with('success', "You're logged out");
+//});
 
-/**
- * Default Hello Route
- */
-Route::get('/', function ()
-{
+
+Route::get('/', function () {
     return View::make('hello');
 });
 
--- 2014/l4demo/app/views/demo/auth/login.blade.php
+++ 2014/l4demo/app/views/demo/auth/login.blade.php
@@ -2,12 +2,24 @@
 
 @section('content')
 
+@if(Session::has('success'))
+<div class="alert alert-success">
+    {{ Session::get('success') }}
+</div>
+@endif
+
+@if(Session::has('error'))
+<div class="alert alert-danger">
+    {{ Session::get('error') }}
+</div>
+@endif
+
+@unless(Auth::check())
 <div class="page-header">
     <h2>Please login</h2>
 </div>
 
-
-<form class="form col-lg-6 col-md-6 col-xs-6 col-sm-6" action="" method="post">
+<form class="form col-lg-6 col-md-6 col-xs-6 col-sm-6" action="login" method="post">
     <div class="form-group">
         <label for="name">E-mail</label>
         <input type="email" class="form-control" id="email" name="email" />
@@ -18,6 +30,16 @@
         <input type="password" class="form-control" id="password" name="password" />
     </div>
 
-    <input type="submit" class="btn btn-primary"/>
+    <input type="submit" class="btn btn-primary" value="Login"/>
 </form>
+@endunless
+@unless(!Auth::check())
+<div class="page-header">
+    <h2>You're logged in as {{ Auth::user()->name }}</h2>
+</div>
+
+<form class="form col-lg-6 col-md-6 col-xs-6 col-sm-6" action="logout" method="post">
+    <input type="submit" class="btn btn-primary" value="Logout"/>
+</form>
+@endunless
 @stop
